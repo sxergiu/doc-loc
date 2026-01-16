6f6f2c2f417247e0cb9e4bc61de02a9a455c7949
sergiu <sergiu.varga@irian.ro>
2026-01-16 20:32:03 +0200

    empty backend app

---
+++ doc-loc-backend/src/main/java/com/cebp/doclocbackend/DocLocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cebp.doclocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocLocBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocLocBackendApplication.class, args);
+    }
+
+}
---
+++ doc-loc-backend/src/test/java/com/cebp/doclocbackend/DocLocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebp.doclocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocLocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
