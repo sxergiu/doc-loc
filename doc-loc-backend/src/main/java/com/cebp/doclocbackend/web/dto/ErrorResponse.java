c4eabba4ee29225f518f9d33a6dc8859c257975d
sergiu <sergiu.varga@irian.ro>
2026-01-17 12:29:26 +0200

    added main dtos

---
+++ doc-loc-backend/src/main/java/com/cebp/doclocbackend/web/dto/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.cebp.doclocbackend.web.dto;
+
+public class ErrorResponse {
+    private String code;
+    private String message;
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
