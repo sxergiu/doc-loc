8bd6bea08cf3f7f182896c537aa0b9468974df5b
sergiu <sergiu.varga@irian.ro>
2026-01-17 12:34:24 +0200

    test config with h2

--- doc-loc-backend/src/test/java/com/cebp/doclocbackend/DocLocBackendApplicationTests.java
+++ doc-loc-backend/src/test/java/com/cebp/doclocbackend/DocLocBackendApplicationTests.java
@@ -2,8 +2,16 @@ package com.cebp.doclocbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:docloc;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class DocLocBackendApplicationTests {
 
     @Test
